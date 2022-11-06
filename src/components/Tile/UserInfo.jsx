07f161207312bc56aa2e2555ad20ef96a141984e
barruTech <tipwindi26@gmail.com>
2022-11-06 10:40:23 +0300

    hhh

--- src/components/Tile/UserInfo.jsx
+++ src/components/Tile/UserInfo.jsx
@@ -7,13 +7,15 @@ const UserInfo = ({ peer, audioEnabled }) => {
   return (
     <div className='wema_card_outer'>
       <div className='wema_card'>
-        <p className='wema_username'>{peer.name[0]}</p>        
-        {audioEnabled ? 
-          <button className='btn_btn'><BsMic/></button> :  <button className='btn_btn'><BsMicMute/></button>}
-        {/* {peer.roleName} */}       
+        <p className='wema_username'>
+          {peer.name[0]}        
+          {audioEnabled ? 
+            <button className='btn_btn'><BsMic/></button> :  <button className='btn_btn'><BsMicMute/></button>}
+          {/* {peer.roleName} */}
+        </p>       
       </div>
       <div className='opacity-50'>
-        <h4>{peer.name}</h4>
+        <h6>{peer.name}</h6>
        
       </div>
     </div>
